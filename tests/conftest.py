cfebc22202d05e254694f94b7b9716988056412f
frmdstryr <frmdstryr@protonmail.com>
2025-07-02 23:07:42 -0400

    tests: Include examples in sys path

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,6 +2,8 @@
 # © 2014 Mark Harviston <mark.harviston@gmail.com>
 # © 2014 Arve Knudsen <arve.knudsen@gmail.com>
 # BSD License
+import os
+import sys
 import logging
 
 from pytest import fixture
@@ -9,6 +11,8 @@ from pytest import fixture
 log_fmt = "%(asctime)s - %(levelname)s - %(name)s - %(message)s"
 logging.basicConfig(level=logging.DEBUG, format=log_fmt)
 
+# Ensure examples are in sys path 
+sys.path.append(os.path.dirname(os.path.dirname(__file__))) 
 
 @fixture(scope="session")
 def application():
